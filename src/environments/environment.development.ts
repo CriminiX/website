0c6373ca8919fab8cec9fefae6a18dc81c9eefe2
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2023-06-02 23:39:01 -0300

    fix api url on dev

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    //url: "http://localhost:8080",
-    url: "http://ec2-34-230-67-231.compute-1.amazonaws.com:8080",
+    url: "http://localhost:8080",
     layout: 2
 };
