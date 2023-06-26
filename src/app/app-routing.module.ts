37718722c05de8daf15a3830ed27844586a62de0
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2023-06-26 19:58:49 -0300

    feat: preload all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,39 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./routes/home/home.module').then(m => m.HomeModule)
-  },
-  {
-    path: 'docs',
-    loadChildren: () => import('./routes/documentation/documentation.module').then(m => m.DocumentationModule)
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./routes/dashboard/dashboard.module').then(m => m.DashboardModule)
-  },
-  {
-    path: 'evaluate',
-    loadChildren: () => import('./routes/evaluate/evaluate.module').then(m => m.EvaluateModule)
-  },
-  {
-    path: '**',
-    redirectTo: 'home'
-  }
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        loadChildren: () => import('./routes/home/home.module').then(m => m.HomeModule)
+    },
+    {
+        path: 'docs',
+        loadChildren: () => import('./routes/documentation/documentation.module').then(m => m.DocumentationModule)
+    },
+    {
+        path: 'dashboard',
+        loadChildren: () => import('./routes/dashboard/dashboard.module').then(m => m.DashboardModule)
+    },
+    {
+        path: 'evaluate',
+        loadChildren: () => import('./routes/evaluate/evaluate.module').then(m => m.EvaluateModule)
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {
+        preloadingStrategy: PreloadAllModules
+    })],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
