b54a9e8f009773a116d37e43922b4cad742586ba
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2023-09-08 23:31:57 -0300

    feat: score summary on feedback with table

--- src/app/routes/evaluate/feedback-evaluate-dialog/feedback-evaluate-dialog.component.ts
+++ src/app/routes/evaluate/feedback-evaluate-dialog/feedback-evaluate-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {FeedbackService} from "../../../shared/services/feedback/feedback.service";
 import {FeedbackEvaluateForm} from "./feedback-evaluate-form";
@@ -8,17 +8,22 @@ import {FeedbackEvaluateDialogContent} from "./feedback-evaluate-dialog-content"
 import {da} from "date-fns/locale";
 import {Feedback} from "../../../shared/models/feedback";
 import {SuggestionsScoresFeedbackEvaluateForm} from "./suggestions-scores-feedback-evaluate-form";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-feedback-evaluate-dialog',
   templateUrl: './feedback-evaluate-dialog.component.html',
   styleUrls: ['./feedback-evaluate-dialog.component.scss']
 })
-export class FeedbackEvaluateDialogComponent implements OnInit {
+export class FeedbackEvaluateDialogComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(MatSort) sort!: MatSort;
 
   loading = false;
   feedbackForm!: FormGroup;
   locations!: FeedbackEvaluateDialogContent[];
+  dataTable!: MatTableDataSource<FeedbackEvaluateDialogContent>;
   suggestionScoresForm!: FormGroup<SuggestionsScoresFeedbackEvaluateForm>[];
 
   constructor(
@@ -30,8 +35,15 @@ export class FeedbackEvaluateDialogComponent implements OnInit {
   ) {
   }
 
+  ngAfterViewInit() {
+    this.dataTable.sort = this.sort;
+  }
+
   ngOnInit(): void {
-    this.locations = this.data;
+    this.locations = this.data.map((loc, index) => {
+      return {indexLocation: index + 1, ...loc};
+    });
+    this.dataTable = new MatTableDataSource(this.locations);
 
     this.suggestionScoresForm = this.locations.map(x => this.formBuilder.group({
       suggestionScore: [0, [Validators.required, Validators.min(-500), Validators.max(500)]],
